8b886d0e6e1d743b700df79005e4bc255ae0136b
wisemonk75@gmail.com <martinflower>
2022-07-18 11:52:57 +0000

    Add Tailwind css

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 const Index = () => {
   return (
     <div>
-      <h1>My Home Page</h1>
+      <h1 className="text-3xl font-bold text-blue-600">My Home Page</h1>
       <p>This my first blog page </p>
       <Link to="/about">A porpos</Link>
     </div>
